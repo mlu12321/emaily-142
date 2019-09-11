ce7c635d51f1e7f3b1e255b8feb2613a8da149ac
Xue Lu <luxueccc@gmail.com>
2019-09-10 21:46:21 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express'); //common js
+const app = express(); // generate an express app
+
+
+app.get('/', (req, res) => {
+  res.send({hi: 'there'});
+}); // 
+
+const PORT = process.env.PORT || 5000; //for heroku
+app.listen(PORT); //tell node to listen to port 5000 
